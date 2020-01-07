5bbc02e0ff697ed23d65dfbd42dbc90b6ae519f0
AndruhaPower <pazit@yandex.ru>
2020-01-07 04:51:09 +0300

    1 и 2 задание

--- objective-c basics/lesson3/lesson3/main.m
+++ objective-c basics/lesson3/lesson3/main.m
@@ -10,8 +10,59 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        // Читает цифры введенные пользователем и разом печатает все введенные данные
+        
+        NSLog(@"Please input a capacity of the array: \n");
+        int capacity;
+        scanf("%d", &capacity);
+        NSMutableArray *numbersArray = [NSMutableArray arrayWithCapacity:capacity];
+        int i = 0;
+        while (i <= capacity) {
+            NSLog(@"\n Input a number");
+            int userNum;
+            scanf("%d", &userNum);
+            NSNumber *number = [NSNumber numberWithInt:userNum];
+            [numbersArray insertObject:number atIndex:i];
+            i++;
+        }
+        NSLog(@"Your numbers are:\n");
+        
+        for (NSNumber *increment in numbersArray) {
+            NSLog(@"%@", increment);
+        }
     }
     return 0;
 }
+
+// Улучшенный калькулятор
+
+enum Operations {
+    addition,
+    subtraction,
+    division,
+    mupliplication
+};
+
+typedef enum Operations Operations;
+
+int calculate(int a, int b, Operations operation) {
+    switch (operation) {
+        case addition:
+            return a+b;
+            break;
+        case subtraction:
+            return a-b;
+            break;
+        case division:
+            return a / b;
+            break;
+        case mupliplication:
+            return a * b;
+    }
+}
+
+// *
+struct User {
+    char name[20];
+};
